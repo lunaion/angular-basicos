935016b1f9b8e49900dce2bd828beaecf4042da6
Javier Luna <lunaion2200@gmail.com>
2021-02-15 22:29:45 -0500

    primer commit

---
+++ src/app/heroes/heore/heroe.component.ts
@@ -0,0 +1,33 @@
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+
+    nombre: string = 'Ironman';
+    edad: number = 34;
+
+    get nombreCapitalizado(): string {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${ this.nombre } - ${ this.edad } `;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 40;
+    }
+
+
+
+}
